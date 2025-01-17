004b6046b56e68c49d003ada64239cde485a154c
Elena Salmon <eas@observatorysciences.co.uk>
2025-01-17 12:17:21 +0000

    Commenting out debug write_to_file lines so doesn't fill up hard drive when used

--- sim/FMB_PLCs.py
+++ sim/FMB_PLCs.py
@@ -1,20 +1,21 @@
-write_to_file(filename, "\nUsing user-input PLC definitions from \'FMB_PLCs.py\'")
+#write_to_file(filename, "\nUsing user-input PLC definitions from \'FMB_PLCs.py\'")
 if plc_num == 6:
-	write_to_file(filename, "\nPLC6 enabled")
+	#write_to_file(filename, "\nPLC6 enabled")
 	self.set_p_var(86, 1)	#StartupStarted
 	self.set_p_var(95, 0)	#ShutdownNotCompleted
 	sleep(2)				#DrivePhasing takes some time
 	self.set_p_var((self.caxis*100 + 17), 1)	#DrivePhased
 	self.set_p_var(96, 1)	#AutoStartupCompleted
 	self.set_p_var(86, 0)	#StartupNotStarted
-elif plc_num == 12:
-	write_to_file(filename, "\nPLC12 enabled")
+#elif plc_num == 12:
+if plc_num == 12:
+	#write_to_file(filename, "\nPLC12 enabled")
 	self.set_p_var(196, 1)	#ShutdownStarted
 	self.set_p_var(96, 0)	#AutoStartupNotCompleted
 	self.set_p_var((self.caxis*100 + 17), 0)	#DriveNotPhased
 	self.set_p_var(95, 1)	#ShutdownCompleted
 	self.set_p_var(196, 0)	#ShutdownNotStarted
-elif 0<=plc_num<=31:
-	write_to_file(filename, "\nenabling of PLC%r requested, but no such PLC program exists"%(plc_num))
-else:
-	write_to_file(filename, "\nenabling of PLC%r requested, but no such PLC program exists as PLC number out of bounds"%(plc_num))
+#elif 0<=plc_num<=31:
+	#write_to_file(filename, "\nenabling of PLC%r requested, but no such PLC program exists"%(plc_num))
+#else:
+	#write_to_file(filename, "\nenabling of PLC%r requested, but no such PLC program exists as PLC number out of bounds"%(plc_num))
--- sim/defaultPLCs.py
+++ sim/defaultPLCs.py
@@ -1,5 +1,5 @@
 logging.debug("Using default PLC definitions from \'defaultPLCs.py\'")
-write_to_file(filename, "\nUsing default PLC definitions from \'defaultPLCs.py\'")
+#write_to_file(filename, "\nUsing default PLC definitions from \'defaultPLCs.py\'")
 if 0<=plc_num<=31:
 	logging.debug("Enabling of PLC{} requested, but no such PLC program exists".format(plc_num))
 else:
